e03d402172d79688768999a0b010e655e165b0eb
Luis Molina <lemolina27@hotmail.com>
2024-02-12 17:59:47 -0500

    Classes modified

--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,2 +1,14 @@
-package com.platzi.platzimarket.persistence.crud;public interface ProductoCrudRepository {
+package com.platzi.platzimarket.persistence.crud;
+
+import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+//cuando se haga la extensión de la interfaz CrudRepository<el primer dato es la tabla, el sgundo dato es el tipo de dato de la llave primaria>
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //Query methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
