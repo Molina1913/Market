5207e5e7c1f485dd290832cdace4af3411dfa506
Luis Molina <lemolina27@hotmail.com>
2024-02-12 17:57:46 -0500

    Classes Product, ProductRepository, ProductService, ProductoRepository added

--- src/main/java/com/platzi/platzimarket/domain/service/ProductService.java
+++ src/main/java/com/platzi/platzimarket/domain/service/ProductService.java
@@ -1,2 +1,36 @@
-package com.platzi.platzimarket.domain.service;public class Productservice {
+package com.platzi.platzimarket.domain.service;
+
+import com.platzi.platzimarket.domain.Product;
+import com.platzi.platzimarket.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return  productRepository.getAll();
+    }
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+    public boolean delete(int productId){
+        if(getProduct(productId).isPresent()){
+            productRepository.delete(productId);
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -1,2 +1,54 @@
-package com.platzi.platzimarket.persistence;public class ProductoRepository {
+package com.platzi.platzimarket.persistence;
+
+import com.platzi.platzimarket.domain.Product;
+import com.platzi.platzimarket.domain.repository.ProductRepository;
+import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
+import com.platzi.platzimarket.persistence.entity.Producto;
+import com.platzi.platzimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository //Le indica a spring que esta clase va a interactuar con la base de datos
+public class ProductoRepository implements ProductRepository {
+    @Autowired
+    private ProductoCrudRepository productoCrudRepository;
+    @Autowired
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarceProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto=mapper.toProduct(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    public void delete(int productId){
+        productoCrudRepository.deleteById(productId);
+    }
 }
